3b3a666896e5d26658f6a3749861995755153c4d
Ariel Zhen <zhenpingping@live.com>
2018-04-28 22:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// function iterativeLog(array, index){
-//   array.forEach(element => {
-//     console.log(`${index}: ${element}`)
-//   });
-// }
-//
-// function iterate(callback){
-//   var array = [0, 2, 4, 6, 8, 10];
-//   array.forEach(callback);
-// }
-//
-// function doToArray(array, callback){
-//   array.forEach(callback);
-// }
+function iterativeLog(array, index){
+  array.forEach(element => {
+    console.log(`${index}: ${element}`)
+  });
+}
+
+function iterate(callback){
+  var array = [0, 2, 4, 6, 8, 10];
+  array.forEach(callback);
+}
+
+function doToArray(array, callback){
+  array.forEach(callback);
+}
