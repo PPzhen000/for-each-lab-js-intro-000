d1f3eb5e8053a056603cd9852c65087b0f2bb877
Ariel Zhen <zhenpingping@live.com>
2018-04-28 22:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 //     console.log(`${index}: ${element}`)
 //   });
 // }
-// 
+//
 // function iterate(callback){
 //   var array = [0, 2, 4, 6, 8, 10];
 //   array.forEach(callback);
 // }
-// 
+//
 // function doToArray(array, callback){
 //   array.forEach(callback);
 // }
